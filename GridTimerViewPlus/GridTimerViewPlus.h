2c869279e725e572eda9661c471d2edd46a28cac
paoloandrea <paolo.rossignoli@gmail.com>
2019-11-08 11:39:28 +0100

    Initial Libs setup

---
+++ GridTimerViewPlus/GridTimerViewPlus.h
@@ -0,0 +1,18 @@
+//
+//  GridTimerViewPlus.h
+//  GridTimerViewPlus
+//
+//  Created by Paolo Rossignoli on 08/11/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GridTimerViewPlus.
+FOUNDATION_EXPORT double GridTimerViewPlusVersionNumber;
+
+//! Project version string for GridTimerViewPlus.
+FOUNDATION_EXPORT const unsigned char GridTimerViewPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GridTimerViewPlus/PublicHeader.h>
+
+
